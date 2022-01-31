b4c152605062fe52b2905af96d8493c22c47ff88
Adam Ciuris <adamciuris@gmail.com>
2022-01-31 00:40:25 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+This is my personal website. This project is a work in progress.
+I am learning and working out the kinks as I go.
+
+My primary goal is to learn how to create and deploy my own website.
---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      This website is a work in progress. I am learning and working out the kinks as I go.
+    <div className='left hand side'>
+    </div>
+    <div className='Navbar'>
+      okay
+    </div>
+    <div className='scrolling part'>
+    </div>
+    </div>
+
+  );
+}
+
+export default App;
